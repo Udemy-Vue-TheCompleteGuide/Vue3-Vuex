f08bf1f9da422627bcf202fb3a4d104c8c9d681d
Marlon Fernandez <marlonfsolis@outlook.com>
2021-01-16 17:38:48 -0500

    Adding and subtracting.

--- src/components/card/CounterCard.vue
+++ src/components/card/CounterCard.vue
@@ -1,22 +1,34 @@
 <template>
   <BaseCard class="w-100">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">
-      Some quick example text to build on the card title
-      and make up the bulk of the card's content.
-    </p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <h3 class="card-title">Counter</h3>
+    <p class="card-text">Counter Value: <span>{{ counter }}</span></p>
+    <div>
+      <button class="btn btn-success" style="margin-right: 5px;" @click="addOne">Add One</button>
+      <button class="btn btn-danger" @click="subtractOne">Subtract One</button>
+    </div>
   </BaseCard>
 </template>
 
 <script>
 import BaseCard from '@/components/card/BaseCard.vue';
 
+import { mapState, mapGetters, mapActions } from 'vuex';
+
 export default {
   name: 'CounterCard',
   components: { BaseCard },
+  computed: {
+    ...mapState(['counter']),
+    ...mapGetters({
+      getCounter: 'getCounter',
+    }),
+  },
+  methods: {
+    ...mapActions({
+      addOne: 'addOne',
+      subtractOne: 'subtractOne',
+    }),
+  },
 };
 </script>
 
